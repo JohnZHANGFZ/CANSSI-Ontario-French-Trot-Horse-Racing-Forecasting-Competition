b8d7eef40aefc521a92d6a0ae77398d6677ebbaa
Fangzheng Zhang <101146860+JohnZHANGFZ@users.noreply.github.com>
2023-11-07 19:55:38 -0500

    Delete model/data_preprocessing.py

--- model/data_preprocessing.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('Holdout.csv')
-
-df['IntGender'] = df['Gender'].map({'F': 2, 'M': 1})
-
-df.to_csv('/Users/fangzhengzhang/Desktop/CANSSI/Holdout.csv', index=False)
